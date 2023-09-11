3eb0bea7e42f9de21699f130075c7b0b113e0dd0
Timea Szucs <szucstimea02@gmail.com>
2023-09-11 20:43:36 +0200

    Rolunk folytatas

--- resources/js/components/About.vue
+++ resources/js/components/About.vue
@@ -1,13 +1,31 @@
 <template>
-    <div class="content">
-        <div class="fejlec d-flex flex-column justify-content-center pt-6 pt-md-7">
+    <div class="tartalom pt-6 pt-md-7">
+        <div id="fejlec d-flex flex-column justify-content-center">
             <h2 class="pb-2 text-center">Rólunk</h2>
             <hr class="m-auto">
             <h3 class="pt-2 text-center">Üdvözöljük webshopunkban!</h3>
         </div>
-    </div>
-    <div>
-
+        <div id="szoveg" class="p-3 pt-5">
+            <div id="part1" class="d-flex">
+                <h5 class="d-flex justify-content-start w-50 pe-3">Családi vállalkozásunk nagy szeretettel és elkötelezettséggel foglalkozik rózsatermesztéssel.
+                    Célunk, hogy ügyfeleinknek olyan rózsákat kínáljunk, amelyek nemcsak a szemet gyönyörködtetik, 
+                     hanem hosszan tartó frissességet és illatot biztosítanak. Munkatársaink szakértelmének és 
+                     gondoskodásának köszönhetően minden rózsa gondosan kiválasztott és gondozott, hogy a lehető 
+                     legjobb minőséget nyújtsa.
+                </h5>
+                <img src="../../assets/kepek/rolunk1.jpg" class="d-flex justify-content-end w-50" alt="rolunk1">
+            </div>
+            <div id="part2" class="d-flex pt-3">
+                <img src="../../assets/kepek/rolunk2.jpg" class="d-flex justify-content-start w-25" alt="rolunk2">
+                <h5 class="d-flex justify-content-end w-75 ps-3">Munkánk során arra törekszünk, hogy minőségi és gyönyörű rózsákat neveljünk,
+                     amelyek a legkiválóbb minőséget képviselik.Vállalkozásunk büszke arra, hogy hosszú évek óta szolgálja ügyfeleinket,
+                      és kiemelkedő minőséget nyújt a rózsák terén. Ügyfeleink elégedettsége az elsődleges célunk, ezért mindig 
+                      törekszünk a legjobb szolgáltatást nyújtani. Bízunk benne, hogy vállalkozásunk elnyeri tetszését, és lehetőséget kapunk 
+                    arra, hogy Önt is üdvözölhessük a vásárlóink körében. Ha további információra vagy kérdésekre van szüksége,
+                     készséggel állunk rendelkezésére!
+                </h5>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -19,17 +37,19 @@
 $szurke_szoveg: #787878
 $rozsaszin: #E4A0B7
 
-.content
+.tartalom
     color: $szurke_szoveg
     background-color: #f5f5f5
 
-    .fejlec
+    #fejlec
         text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25)
         font-family: Inria Serif
 
     hr
         border: solid 0.2rem $rozsaszin
         width: 10rem
+
+
 
 
 </style>

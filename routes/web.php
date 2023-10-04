28181b49b576661fd909f7079f6ec567432a020f
Csonka Csaba <csonkacsaba1986gmail.com>
2023-10-04 21:13:12 +0200

    Routes módosítás

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UzletController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;

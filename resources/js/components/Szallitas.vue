1db9aabeeae04dd28fbf013782f092f2bfa6139c
Timea Szucs <szucstimea02@gmail.com>
2024-02-07 21:26:16 +0100

    fizetés hozzáadás + üres kosár érzékelés

--- resources/js/components/Szallitas.vue
+++ resources/js/components/Szallitas.vue
@@ -12,60 +12,68 @@ const cartData = ShoppingCart();
         <div class="col nav-item active">Szállítás</div>
         <div class="col nav-item">Megrendelés</div>
     </div>
-    <div id="szallitas-content" class="container-fluid d-flex row p-md-0">
-        <div class="flex-grow-1 col-12 col-sm-7 col-md-7 col-lg-8">
-            <div class="title my-3">Szállítási adatok</div>
-            <section class="szallitasi-adatok">
-                    <div class="row my-3" id="address">
-                        <div class="col d-inline-flex">
-                            <div class="checkbox-wrapper-21">
-                                <label class="control control--checkbox ps-5 container" for="othAdd">
-                                    Számlázási címtől eltérő szállítási cím.
-                                    <input type="checkbox" id="othAdd" value="othAdd" v-model="cartData.shippingAddress.new" true-value="yes" false-value="no" name="othAdd"/>
-                                    <div class="control__indicator ms-3"></div>
-                                </label>
+    <div v-if="cartData.cartItems.length > 0 ">
+        <div id="szallitas-content" class="container-fluid d-flex row p-md-0">
+            <div class="flex-grow-1 col-12 col-sm-7 col-md-7 col-lg-8">
+                <div class="title my-3">Szállítási adatok</div>
+                <section class="szallitasi-adatok">
+                        <div class="row my-3" id="address">
+                            <div class="col d-inline-flex">
+                                <div class="checkbox-wrapper-21">
+                                    <label class="control control--checkbox ps-5 container" for="othAdd">
+                                        Számlázási címtől eltérő szállítási cím.
+                                        <input type="checkbox" id="othAdd" value="othAdd" v-model="cartData.shippingAddress.new" true-value="yes" false-value="no" name="othAdd"/>
+                                        <div class="control__indicator ms-3"></div>
+                                    </label>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="mb-3">
-                        <form id="shipping-form" action="http://localhost/webshop_rozsatveszek/public/megrendeles" class="d-lg-inline-flex">
-                            <div class="d-inline-flex flex-column">
-                                <div class="row d-inline-flex center mb-2">
-                                    <label for="cart-lastname" class="p-1 col text-end">Név</label>
-                                    <input id="cart-lastname" v-if="cartData.shippingAddress.new == 'no'" type="text" v-model="cartData.billingAddress.name" class="form-control ms-3 col" disabled required/>
-                                    <input id="cart-lastname" v-else type="text" v-model="cartData.shippingAddress.name" class="form-control ms-3 col" required/>
-                                </div>
-                                <div class="row d-inline-flex center mb-2">
-                                    <label for="cart-zipcode" class="p-1 col text-end">Irányítószám</label>
-                                    <input id="cart-zipcode" v-if="cartData.shippingAddress.new == 'no'" type="number" v-model="cartData.billingAddress.zipCode" class="form-control ms-3 col" disabled required/>
-                                    <input id="cart-zipcode" v-else type="number" v-model="cartData.shippingAddress.zipCode" class="form-control ms-3 col" required/>
-                                </div>
-                                <div class="row d-inline-flex center mb-2">
-                                    <label for="cart-city" class="p-1 col text-end">Település</label>
-                                    <input id="cart-city" v-if="cartData.shippingAddress.new == 'no'" type="text" v-model="cartData.billingAddress.city" class="form-control ms-3 col" disabled required/>
-                                    <input id="cart-city" v-else type="text" v-model="cartData.shippingAddress.city" class="form-control ms-3 col" required/>
-                                </div>
-                                <div class="row d-inline-flex center mb-2">
-                                    <label for="cart-street" class="p-1 col text-end">Utca</label>
-                                    <input id="cart-street" v-if="cartData.shippingAddress.new == 'no'" type="text" v-model="cartData.billingAddress.street" class="form-control ms-3 col" disabled required/>
-                                    <input id="cart-street" v-else type="text" v-model="cartData.shippingAddress.street" class="form-control ms-3 col" required/>
-                                </div>
-                                <div class="row d-inline-flex center mb-2">
-                                    <label for="cart-house" class="p-1 col text-end">Házszám</label>
-                                    <input id="cart-house" v-if="cartData.shippingAddress.new == 'no'" type="text" v-model="cartData.billingAddress.house" class="form-control ms-3 col" disabled required/>
-                                    <input id="cart-house" v-else type="text" v-model="cartData.shippingAddress.house" class="form-control ms-3 col" required/>
-                                </div>
-                            </div>                            
-                        </form>
-                    </div>
-            </section>
+                        <div class="mb-3">
+                            <form id="shipping-form" action="http://localhost/webshop_rozsatveszek/public/megrendeles" class="d-lg-inline-flex">
+                                <div class="d-inline-flex flex-column">
+                                    <div class="row d-inline-flex center mb-2">
+                                        <label for="cart-lastname" class="p-1 col text-end">Név</label>
+                                        <input id="cart-lastname" v-if="cartData.shippingAddress.new == 'no'" type="text" v-model="cartData.billingAddress.name" class="form-control ms-3 col" disabled required/>
+                                        <input id="cart-lastname" v-else type="text" v-model="cartData.shippingAddress.name" class="form-control ms-3 col" required/>
+                                    </div>
+                                    <div class="row d-inline-flex center mb-2">
+                                        <label for="cart-zipcode" class="p-1 col text-end">Irányítószám</label>
+                                        <input id="cart-zipcode" v-if="cartData.shippingAddress.new == 'no'" type="number" v-model="cartData.billingAddress.zipCode" class="form-control ms-3 col" disabled required/>
+                                        <input id="cart-zipcode" v-else type="number" v-model="cartData.shippingAddress.zipCode" class="form-control ms-3 col" required/>
+                                    </div>
+                                    <div class="row d-inline-flex center mb-2">
+                                        <label for="cart-city" class="p-1 col text-end">Település</label>
+                                        <input id="cart-city" v-if="cartData.shippingAddress.new == 'no'" type="text" v-model="cartData.billingAddress.city" class="form-control ms-3 col" disabled required/>
+                                        <input id="cart-city" v-else type="text" v-model="cartData.shippingAddress.city" class="form-control ms-3 col" required/>
+                                    </div>
+                                    <div class="row d-inline-flex center mb-2">
+                                        <label for="cart-street" class="p-1 col text-end">Utca</label>
+                                        <input id="cart-street" v-if="cartData.shippingAddress.new == 'no'" type="text" v-model="cartData.billingAddress.street" class="form-control ms-3 col" disabled required/>
+                                        <input id="cart-street" v-else type="text" v-model="cartData.shippingAddress.street" class="form-control ms-3 col" required/>
+                                    </div>
+                                    <div class="row d-inline-flex center mb-2">
+                                        <label for="cart-house" class="p-1 col text-end">Házszám</label>
+                                        <input id="cart-house" v-if="cartData.shippingAddress.new == 'no'" type="text" v-model="cartData.billingAddress.house" class="form-control ms-3 col" disabled required/>
+                                        <input id="cart-house" v-else type="text" v-model="cartData.shippingAddress.house" class="form-control ms-3 col" required/>
+                                    </div>
+                                </div>                            
+                            </form>
+                        </div>
+                </section>
+            </div>
+            <ShoppingCartSide />        
         </div>
-        <ShoppingCartSide />        
+        <div class="container">
+            <div class="row mt-3 align-items-center">
+                <div class="col-6 d-flex justify-content-start"><a href="szamlazas"><button type="button" class="btn btn-vasarlas m-0">Vissza</button></a></div>
+                <div class="col-6 d-flex justify-content-end"><button type="submit" form="shipping-form" class="btn btn-vasarlas m-0">Folytatás<font-awesome-icon :icon="['fas', 'angle-right']" class="ps-1" /></button></div>
+            </div>
+        </div>
     </div>
-    <div class="container">
-        <div class="row mt-3 align-items-center">
-            <div class="col-6 d-flex justify-content-start"><a href="szamlazas"><button type="button" class="btn btn-vasarlas m-0">Vissza</button></a></div>
-            <div class="col-6 d-flex justify-content-end"><button type="submit" form="shipping-form" class="btn btn-vasarlas m-0">Folytatás<font-awesome-icon :icon="['fas', 'angle-right']" class="ps-1" /></button></div>
+    <div v-else class="container m-auto">
+        <div class="row kosar-ures">A kosár üres.</div>
+        <div class="row vasarlas">
+            <a href="termekek"><button type="button" class="btn btn-vasarlas m-0">Vásárlás</button></a>
         </div>
     </div>
 </template>
